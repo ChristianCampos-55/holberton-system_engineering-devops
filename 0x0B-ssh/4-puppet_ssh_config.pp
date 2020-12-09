d39ad284637571090ee230f5ac40d66068d94bb4
ChristianCampos-55 <1566@holbertonschool.com>
2020-12-09 03:30:55 +0000

    [FIX] Misng pass

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,14 +1,12 @@
 #SSH config with puppet
-file { '/etc/ssh/ssh_config':
-  ensure  => 'file',
-  path	  => '/etc/ssh/ssh_config',
-  line	  => '	IdentityFile ~/.ssh/holberton',
-  replace =>true,
+file_line { 'Disable password authentication':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no'
 }
 
-file { '/etc/ssh/ssh_config':
-  ensure  => 'file',
-  path	  => '/etc/ssh/ssh_config',
-  line	  => '	PasswordAuthentication no',
-  replace =>true,
+file_line { 'Declare file':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdentityFile ~/.ssh/holberton',
+  replace => true,
 }
