89027be4d93fe7b9879684c703238aa73e28a672
ChristianCampos-55 <1566@holbertonschool.com>
2020-12-22 05:05:13 +0000

    [FIX] Syntax error

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -2,5 +2,5 @@
 #Deploys a responce, not-found page with puppet
 exec { 'Server config':
   provider => shell,
-  command  => 'sudo apt-get -y update; sudo apt-get -y install nginx; echo "Holberton School" > /var/www/html/index.html ;  sed -i "/server_name _;/a location /redirect_me https://www.youtube.com/watch?v=ms2BvRbjOYo permanent;\n\tadd_header X-Served-By \$hostname;/" /etc/nginx/sites-available/default ; service nginx start'
+  command  => 'sudo apt-get -y update; sudo apt-get -y install nginx; echo "Holberton School" > /var/www/html/index.html ;  sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me http:\/\/https://www.youtube.com/watch?v=ms2BvRbjOYo permanent;\n\tadd_header X-Served-By \$hostname;/" /etc/nginx/sites-available/default ; service nginx start'
 }
