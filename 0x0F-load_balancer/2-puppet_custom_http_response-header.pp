61e11162831c47e0c02cb56746eb6ddda3fb3906
ChristianCampos-55 <1566@holbertonschool.com>
2020-12-22 05:01:38 +0000

    [ADD] ADV TSK

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -0,0 +1,5 @@
+#Deploys a responce, not-found page with puppet
+exec { 'Server config':
+  provider => shell,
+  command  => 'sudo apt-get -y update; sudo apt-get -y install nginx; echo "Holberton School" > /var/www/html/index.html ;  sed -i "/server_name _;/a location /redirect_me https://www.youtube.com/watch?v=ms2BvRbjOYo permanent;\n\tadd_header X-Served-By \$hostname;/" /etc/nginx/sites-available/default ; service nginx start'
+}
