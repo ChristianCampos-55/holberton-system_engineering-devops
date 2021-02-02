a544b257d629aeb1714c89b97a967cc4a19d55e1
ChristianCampos-55 <1566@holbertonschool.com>
2021-02-01 22:23:31 -0500

    [ADD]Folder and TSK 1

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Makes a request to an API"""
+import json
+import requests
+import sys
+
+
+if __name__ == '__main__':
+
+    emp = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
+                       format(sys.argv[1]))
+    name = emp.json().get('name')
+    user = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.
+                        format(sys.argv[1]))
+    jdaughters = user.json()
+    finished = 0
+    count = 0
+    for i in jdaughters:
+        count += 1
+        if i.get('completed'):
+            finished += 1
+
+    print('Employee {} is done with tasks({}/{}):'.
+          format(name, count, finished))
+    for i in jdaughters:
+        if i.get('completed'):
+            print('\t {}'.format(i.get('title')))
