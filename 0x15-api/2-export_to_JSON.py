a752a30ea56bd5b63529ce49cbc8a202f52ea958
ChristianCampos-55 <1566@holbertonschool.com>
2021-02-01 22:49:29 -0500

    [ADD]TSK 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Makes a request to an API"""
+import requests
+import json
+import sys
+
+
+if __name__ == '__main__':
+    emp = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
+                       format(sys.argv[1]))
+    usr_name = emp.json().get('username')
+    user = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.
+                        format(sys.argv[1]))
+    jdaughters = user.json()
+    exp = {}
+    exp['{}'.format(sys.argv[1])] = []
+    for i in jdaughters:
+        exp['{}'.format(sys.argv[1])].append({
+            'task': i.get('title'),
+            'completed': i.get('completed'),
+            'username': usr_name
+        })
+    with open('{}.json'.format(sys.argv[1]), 'w') as file:
+        json.dump(exp, file)
