cbfb1f1adac8e6bf502a86e287a08181a0438a26
ChristianCampos-55 <1566@holbertonschool.com>
2021-02-01 22:40:16 -0500

    [ADD]TSK 2

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Makes a request to an API"""
+import requests
+import csv
+import sys
+
+
+if __name__ == '__main__':
+    emp = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
+                       format(sys.argv[1]))
+    usr_name = emp.json().get('username')
+    user = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.
+                        format(sys.argv[1]))
+    jdaughters = user.json()
+    with open('{}.csv'.format(sys.argv[1]), mode='w') as file:
+        writer = csv.writer(file, delimiter=',',
+                            quotechar='"', quoting=csv.QUOTE_ALL)
+        for i in jdaughters:
+            writer.writerow([sys.argv[1], usr_name, i.get('completed'),
+                             i.get('title')])
