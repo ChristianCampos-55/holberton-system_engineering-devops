98c5f8166de06ad390a088695841e8b549e02e61
ChristianCampos-55 <1566@holbertonschool.com>
2021-02-04 14:36:18 -0500

    [FIX]Typos and logic order

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,7 +7,7 @@ def recurse(subreddit, hot_list=[], after=''):
     """ Prints titles of first 10 posts listed """
     url = 'https://www.reddit.com/r/{}/hot.json?after={}'.format(subreddit,
                                                                  after)
-    req_headers = {'user-agent': 'chris'}
+    req_headers = {'User-agent': 'chris'}
     try:
         reddit_req = requests.get(url, headers=req_headers,
                                   allow_redirects=False)
