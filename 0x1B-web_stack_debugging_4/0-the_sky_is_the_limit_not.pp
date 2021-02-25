bbe2090b259226012e312a8ad83bb05a37c77f66
ChristianCampos-55 <1566@holbertonschool.com>
2021-02-25 00:28:56 -0500

    [FIX]Chek4

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # Fix debugging issue
 exec { 'skies nginx limit':
-  command => 'sudo sed -i 's/15/5000/g' /etc/default/nginx; service nginx restart',
+  command => 'sudo sed -i "s/15/5000/g" /etc/default/nginx; service nginx restart',
   path    => '/bin:/usr/bin:/usr/sbin:/sbin',
 }
