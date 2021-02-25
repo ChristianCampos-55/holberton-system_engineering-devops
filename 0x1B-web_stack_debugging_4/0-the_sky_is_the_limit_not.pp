50a5b5ec389dbed3240d2ab1b60e45283938a769
ChristianCampos-55 <1566@holbertonschool.com>
2021-02-24 21:50:36 -0500

    [ADD]Proj folder and TSK 1

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,8 @@
+# Fix debugging issue
+exec { 'skies nginx limit':
+  command => 'sudo sed -i 's/15/5000/g' /etc/default/nginx;',
+  path    => '/bin:/usr/bin:/usr/sbin:/sbin',
+}
+exec { 'normal restart':
+   command  => 'sudo service nginx restart',
+}
